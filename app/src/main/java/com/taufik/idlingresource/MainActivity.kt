cd523acdf0cb6263cbe9f883ad2c5daed5e8f21d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-05-20 06:03:36 +0700

    Finishing up testing delay 1 idling resource.

--- app/src/main/java/com/taufik/idlingresource/MainActivity.kt
+++ app/src/main/java/com/taufik/idlingresource/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.taufik.idlingresource
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.idlingresource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setButton()
+    }
+
+    private fun setButton() {
+        binding.apply {
+            button.setOnClickListener {
+                delay1()
+            }
+        }
+    }
+
+    private fun delay1() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.apply {
+                textView.text = getString(R.string.delay1)
+            }
+        }, 2000)
     }
 }
